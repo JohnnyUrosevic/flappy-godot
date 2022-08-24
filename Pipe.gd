bbfb29515b7660fb3f3a68767befc410bcc70051
Johnny Urosevic <TheMagicalCake@gmail.com>
2022-08-24 14:11:41 -0700

    Free pipes when they leave the screen

--- Pipe.gd
+++ Pipe.gd
@@ -4,3 +4,7 @@ export var SPEED = -1
 
 func _physics_process(delta):
 	var _collision = move_and_collide(Vector2(SPEED, 0) * delta)
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
