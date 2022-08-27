bd7bdcb19012b0d2b48179c37cda62c06e092135
Johnny <themagicalcake@gmail.com>
2022-08-27 02:01:03 -0700

    Game over and restart

---
+++ Game.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+
+var game_over = false
+var world = load("res://World.tscn")
+
+var current_world = null
+
+func _ready():
+	var _e = Events.connect("game_over", self, "_on_game_over")
+	current_world = world.instance()
+	self.add_child(current_world)
+
+func _on_game_over():
+	game_over = true
+
+func _input(event):
+	if game_over and event.is_action_pressed("ui_accept"):
+		game_over = false
+		current_world.queue_free()
+
+		call_deferred("restart")
+
+func restart():
+	current_world = world.instance()
+	self.add_child(current_world)
+	Score.score = 0
+	get_tree().paused = false
--- World.gd
+++ World.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
+var game_over = load("res://GameOverUI.tscn")
+
 func _ready():
-    var _e = Events.connect("game_over", self, "_on_game_over")
+	var _e = Events.connect("game_over", self, "_on_game_over")
 
 func _on_game_over():
-    get_tree().paused = true
+	$GameOver.show()
+	$Score.global_position.y = $GameOver.rect_global_position.y
+	get_tree().paused = true
