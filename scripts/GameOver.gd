6377a2ff29085c01095ce3bb9fdb488b802be05e
Johnny <themagicalcake@gmail.com>
2022-08-27 02:43:23 -0700

    Add scripts to subfolder

--- scripts/GameOver.gd
+++ scripts/GameOver.gd
@@ -2,11 +2,10 @@ extends Panel
 
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
-		get_tree().paused = false
-		var _r = get_tree().reload_current_scene()
 		Score.score = 0
 		call_deferred("restart")
 
 func restart():
 	var _r = get_tree().reload_current_scene()
+	get_tree().paused = false
 	queue_free()
